bff62313af11c3e7aa8661e3ea37019dc9b996f1
Rodger Combs <rodger.combs@gmail.com>
2017-11-11 08:24:21 -0600

    Add support for 280-character tweets

--- lib/twitter_ebooks/model.rb
+++ lib/twitter_ebooks/model.rb
@@ -142,7 +142,7 @@ module Ebooks
       if path.split('.')[-1] == "json"
         log "Reading json corpus from #{path}"
         lines = JSON.parse(content).map do |tweet|
-          tweet['text']
+          tweet['text'] || tweet['full_text']
         end
       elsif path.split('.')[-1] == "csv"
         log "Reading CSV corpus from #{path}"
@@ -205,7 +205,7 @@ module Ebooks
         if path.split('.')[-1] == "json"
           log "Reading json corpus from #{path}"
           l = JSON.parse(content).map do |tweet|
-            tweet['text']
+            tweet['text'] || tweet['full_text']
           end
           lines.concat(l)
         elsif path.split('.')[-1] == "csv"
--- lib/twitter_ebooks/version.rb
+++ lib/twitter_ebooks/version.rb
@@ -1,3 +1,3 @@
 module Ebooks
-  VERSION = "3.1.6"
+  VERSION = "3.2.0"
 end
